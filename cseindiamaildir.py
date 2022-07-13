ba1d2bb01358521987797edf18fc1b9ccc5356f1
Visuwesh <visuweshm@gmail.com>
2022-07-13 21:03:14 +0530

    Add fetcher for reports
    * cseindiamaildir.py (msg_to_addr): New helper function.
    (articles_in_press_release): Renamed from...
    (articles_in_soup): ... here.
    (push_press_release): Renamed from...
    (push_message): ... here.
    (subpage): Generalise it.
    (articles_in_report, push_report): Functions to fetch reports.
    (REPORT_PDF_RE): Regexp to match PDF of the report.
    (do): Generalise it.

--- cseindiamaildir.py
+++ cseindiamaildir.py
@@ -3,38 +3,44 @@
 #
 # Licensed under the BSD 2-Clause License.
 #
-# Currently this only handles press releases.
+# This file can really use some generalisation of functions.
 
 import bs4
+from datetime import datetime
 import email.message as msg
-from email.utils import formatdate as msgdate
+import email.utils
 import mailbox
 import json
 from html2text import html2text
 import os
+import re
 import threading
 import urllib.request as req
 
 UA = { "User-Agent": "Chrome/96.0.4664.110" }
 THREADS = []
-MAILDIR = mailbox.Maildir(dirname="./test/cse")
+MAILDIR = mailbox.Maildir(dirname="/home/viz/mail/rss2email/scimag")
+REPORT_PDF_RE = re.compile(".*/downloadreports/.*")
 DB = {}
 if os.path.exists("./DB"):
     with open("./DB") as f:
         DB = json.loads(f.read())
 if not DB.get("press-release"):
     DB["press-release"] = []
+if not DB.get("reports"):
+    DB["reports"] = []
+
+# Mail utils
+def msg_to_addr():
+    return "<" + os.getenv("USER") + "@" + os.uname()[1] + ">"
+
+
+
+# Request utils
 
 def request(url):
     """Request URL."""
     return req.urlopen(req.Request(url, headers=UA))
-
-def articles_in_soup(soup):
-    return [
-        { "topic": i.find("ul", class_="article-meta").a.text,
-          "url": i.find("h4").a["href"] }
-        for i in soup.find_all("article")
-    ]
 
 def pages(soup):
     return [
@@ -48,54 +54,99 @@ def content(article):
     return (soup.find("title").text,
             soup.find("div", class_="content-para"))
 
-def push_message(article):
+def subpage(page, mailbox, article_fun, push_fun):
+    """PAGE maybe be a soup object or a string representing a URL.
+    Return the number of article fetched and the total number of
+    articles.
+
+    """
+    n = 0
+    tot = 0
+    if isinstance(page, str):
+        page = bs4.BeautifulSoup(request(page), "html.parser")
+    for i in article_fun(page):
+        tot += 1
+        if i["url"] in DB.get(mailbox, []):
+            continue
+        t = threading.Thread(target=push_fun, args=(i,), name="article fetch " + i["url"])
+        THREADS.append(t)
+        t.run()
+        n += 1
+    return n, tot
+
+
+# Press releases
+
+def articles_in_press_release(soup):
+    return [
+        { "topic": i.find("ul", class_="article-meta").a.text,
+          "url": i.find("h4").a["href"] }
+        for i in soup.find_all("article")
+    ]
+
+def push_press_release(article):
     m = msg.EmailMessage()
     m.add_header("From", "CSE Press Release <press-release@cse>")
-    m.add_header("To", "<" + os.getenv("USER") + "@" + os.uname()[1] + ">")
+    m.add_header("To", msg_to_addr())
     m.add_header("Topic", article["topic"])
     m.add_header("Newsgroups", "CSE-Press-Release")
     m.add_header("Url", article["url"])
     c = content(article["url"])
     m.add_header("Subject", c[0])
     # Poor man's date.
-    m.add_header("Date", msgdate())
+    m.add_header("Date", email.utils.formatdate())
     m.set_content(str(c[1]), subtype="html")
     m.add_alternative(html2text(str(c[1])))
-    print(MAILDIR.add(m))
+    print(MAILDIR.add(m), article["url"])
     DB["press-release"].append(article["url"])
 
+
 
-def subpage(page):
-    """PAGE maybe be a soup object or a string representing a URL.
-    Return the number of article fetched.
+# Reports
+def articles_in_report(soup):
+    return [
+        { "date": i.find(class_="date").text.strip(),
+          "topic": i.find(class_="small-heading").text.strip(),
+          "url": i.find("p", class_="amplitude").a["href"],
+          "pdf": i.find("a", href=REPORT_PDF_RE)["href"] }
+        for i in soup.find_all("div", class_="info")
+    ]
 
-    """
-    n = 0
-    if isinstance(page, str):
-        page = bs4.BeautifulSoup(request(page), "html.parser")
-    for i in articles_in_soup(page):
-        if i["url"] in DB.get("press-release", []):
-            continue
-        t = threading.Thread(target=push_message, args=(i,), name="article fetch " + i["url"])
-        THREADS.append(t)
-        t.run()
-        n += 1
-    return n
+def push_report(report):
+    m = msg.EmailMessage()
+    m.add_header("From", "CSE Reports <reports@cse>")
+    m.add_header("To", msg_to_addr())
+    m.add_header("Topic", report["topic"])
+    m.add_header("Newsgroups", "CSE-Reports")
+    m.add_header("Url", report["url"])
+    m.add_header("Pdf", report["pdf"])
+    m.add_header("Date", email.utils.format_datetime(datetime.strptime(report["date"], "%d %B, %Y")))
+    c = content(report["url"])
+    m.add_header("Subject", c[0])
+    m.set_content(str(c[1]) + "\nPDF: " + report["pdf"], subtype="html")
+    m.add_alternative(html2text(str(c[1])) + "\nPDF: " + report["pdf"])
+    print(MAILDIR.add(m), report["url"])
+    DB["reports"].append(report["url"])
 
-def do():
-    main = bs4.BeautifulSoup(request("https://www.cseindia.org/press-releases"), "html.parser")
+
+
+def do(url, cat, article_fun, push_fun):
+    main = bs4.BeautifulSoup(request(url), "html.parser")
     # Try the mainpage first.
-    n = subpage(main)
+    n, tot = subpage(main, cat, article_fun, push_fun)
     # If no article fetched or if no. articles fetched is less than
     # the number in page, bail.
-    if not (n == 0 or n < len(articles_in_soup(main))):
+    print("n, tot ", n, tot)
+    if not (n == 0 or n < tot):
         p = pages(main)
         for i in p:
-            t = threading.Thread(target=subpage, args=(i,), name=f"subpage {i}")
+            t = threading.Thread(target=subpage, args=(i, cat, article_fun, push_fun),
+                                 name=f"subpage {i}")
             THREADS.append(t)
             t.run()
 
 if __name__ == "__main__":
-    do()
+    do("https://www.cseindia.org/press-releases", "press-release", articles_in_press_release, push_press_release)
+    do("https://www.cseindia.org/reports", "reports", articles_in_report, push_report)
     with open("./DB", "w") as f:
         f.write(json.dumps(DB, indent=4))
